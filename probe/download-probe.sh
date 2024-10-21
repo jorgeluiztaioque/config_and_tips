b1a3ec89b67cd552d986afc375a0622a96133e10
Jorge Luiz Taioque (Japoneis) <jorgeluiztaioque@gmail.com>
2024-10-21 16:11:46 -0300

    Update download-probe.sh
    ajust delete pppoe-connect

--- probe/download-probe.sh
+++ probe/download-probe.sh
@@ -3,6 +3,7 @@
 # Download Probe
 
 echo "$1" | sudo -S rm -rf /opt/teste-speed.py
+echo "$1" | sudo -S rm -rf /opt/pppoe-connect.sh
 cd /opt
 echo "$1" | sudo -S wget https://raw.githubusercontent.com/jorgeluiztaioque/config_and_tips/refs/heads/master/probe/teste-speed.py
 echo "$1" | sudo -S chmod +x teste-speed.py
