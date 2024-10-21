354e5bfee89913ef6cae3af029ecca95107e1a62
Jorge Luiz Taioque (Japoneis) <jorgeluiztaioque@gmail.com>
2024-10-21 16:06:24 -0300

    Update download-probe.sh
    ajust crontab

--- probe/download-probe.sh
+++ probe/download-probe.sh
@@ -9,5 +9,5 @@ echo "$1" | sudo -S chmod +x teste-speed.py
 echo "$1" | sudo -S wget https://raw.githubusercontent.com/jorgeluiztaioque/config_and_tips/refs/heads/master/probe/pppoe-connect.sh
 echo "$1" | sudo -S chmod +x pppoe-connect.sh
 crontab -r
-sudo crontab -l | { cat; echo "*/15 * * * * /opt/teste-speed.py"; } | crontab -
+sudo echo "*/15 * * * * /opt/teste-speed.py" | crontab -
 sudo crontab -l | { cat; echo "*/5 * * * * /opt/pppoe-connect.sh"; } | crontab -
