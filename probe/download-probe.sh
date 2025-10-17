8fa30867cdbdbb8fc4ec8f926ceb6aa4a973b95e
Jorge Luiz Taioque (Japoneis) <jorgeluiztaioque@gmail.com>
2025-10-17 11:52:00 -0300

    Update crontab entries with new password

--- probe/download-probe.sh
+++ probe/download-probe.sh
@@ -11,8 +11,8 @@ echo "$1" | sudo -S chmod +x teste-speed.py
 #echo "$1" | sudo -S chmod +x pppoe-connect.sh
 crontab -r
 sudo crontab -r
-echo "$1" | sudo -S echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" | sudo crontab -
-echo "$1" | sudo -S echo "*/15 * * * * /opt/teste-speed.py" | sudo crontab -
-echo "$1" | sudo -S crontab -l | { cat; echo "*/5 * * * * /opt/pppoe-connect.sh"; } | sudo crontab -
+echo "3n6%r3de" | sudo -S echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" | sudo crontab -
+echo "3n6%r3de" | sudo -S crontab -l | { cat; echo "*/15 * * * * /opt/teste-speed.py"; } | sudo crontab -
+echo "3n6%r3de" | sudo -S crontab -l | { cat; echo "*/5 * * * * /opt/pppoe-connect.sh"; } | sudo crontab -
 varHostname=$(hostname)
 sudo sed -i "s/PB-A01-LEP001-SP/$varHostname/" /opt/teste-speed.py
