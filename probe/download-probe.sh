aa01b38aed1ae198b0f453432cd61b139d92165e
Jorge Luiz Taioque (Japoneis) <jorgeluiztaioque@gmail.com>
2025-10-17 11:42:37 -0300

    Modify download-probe.sh to focus on teste-speed.py
    Updated the download script to remove references to pppoe-connect.sh and adjusted comments accordingly.

--- probe/download-probe.sh
+++ probe/download-probe.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Download Probe
+# Download or update Probe
 
 echo "$1" | sudo -S rm -rf /opt/teste-speed.py
-echo "$1" | sudo -S rm -rf /opt/pppoe-connect.sh
+#echo "$1" | sudo -S rm -rf /opt/pppoe-connect.sh
 cd /opt
 echo "$1" | sudo -S wget https://raw.githubusercontent.com/jorgeluiztaioque/config_and_tips/refs/heads/master/probe/teste-speed.py
 echo "$1" | sudo -S chmod +x teste-speed.py
-echo "$1" | sudo -S wget https://raw.githubusercontent.com/jorgeluiztaioque/config_and_tips/refs/heads/master/probe/pppoe-connect.sh
-echo "$1" | sudo -S chmod +x pppoe-connect.sh
+#echo "$1" | sudo -S wget https://raw.githubusercontent.com/jorgeluiztaioque/config_and_tips/refs/heads/master/probe/pppoe-connect.sh
+#echo "$1" | sudo -S chmod +x pppoe-connect.sh
 crontab -r
 sudo crontab -r
+echo "$1" | sudo -S echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" | sudo crontab -
 echo "$1" | sudo -S echo "*/15 * * * * /opt/teste-speed.py" | sudo crontab -
 echo "$1" | sudo -S crontab -l | { cat; echo "*/5 * * * * /opt/pppoe-connect.sh"; } | sudo crontab -
 varHostname=$(hostname)
